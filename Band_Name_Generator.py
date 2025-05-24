85c87f8e60abcdd6bbbfbfe709cfc9ff49bdc984
Satish <7506satish@gmail.com>
2025-05-24 23:39:44 +0530

    Project uses concatination technique to generate band names

---
+++ Band_Name_Generator.py
@@ -0,0 +1,9 @@
+import time
+City_name = input("\nHey there, Nice to see you!\n Please Enter the name of the City where you grew up: ")
+print(f"\nHey, Even i am from {City_name}")
+Pet_name = input("Please Enter the name of your beloved pet: ")
+print(f"Hmmm....")
+time.sleep(2)
+print(f"I'm thinking....")
+time.sleep(3)
+print(f"\nYour Band Name Could be: {City_name} {Pet_name}\n")
